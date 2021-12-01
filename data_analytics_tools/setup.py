d0bff7014ac16bd3feef610e21dd126c6e380dad
Raul Macedo Fuzita <raul_fuzita@hotmail.com>
2021-12-01 23:22:31 +0000

    Package for Data Analytics
    Package created by Raul Fuzita to perform max and min normalization, z score, inverted skewness, and use pivot with melt.

---
+++ data_analytics_tools/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='DataAnalyticsTools',
+      version='1.0',
+      description='Python Distribution Utilities for Data Analytics',
+      author='Raul Macedo Fuzita',
+      author_email='raul.fuzita@gmail.com',
+      packages=['data_analytics'],
+      package_dir={'data_analytics': 'src/data_analytics'}
+)
---
+++ data_analytics_tools/src/data_analytics_tools/pivot_melt.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+# Author: Raul Macedo Fuzita
+# Date: 01/12/2021
+# All rights reserved
+def pivot_melt(data, column, value, fill=0, filter=None):
+  if (type(filter) is list) & (len(filter) > 0):
+    data = data[np.isin(data, filter)]
+  colnames = data.loc[:,column].unique()
+  newdf = pd.DataFrame()
+  for name in colnames:
+    col = data[data[column] == name][value]
+    temp = pd.DataFrame({name:col.values})
+    newdf = pd.concat([newdf, temp], axis=1)
+  newdf.fillna(fill, inplace=True)
+  return newdf
