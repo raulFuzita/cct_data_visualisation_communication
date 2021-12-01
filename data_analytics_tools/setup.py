f6442cef2746ff904ecf69276e8b12fb5a5ece64
Raul Macedo Fuzita <raul_fuzita@hotmail.com>
2021-12-01 23:50:32 +0000

    Update setup.py

--- data_analytics_tools/setup.py
+++ data_analytics_tools/setup.py
@@ -3,13 +3,8 @@
 from setuptools import setup, find_packages
 from distutils.util import convert_path
 
-main_ns = {}
-ver_path = convert_path('src/data_analytics_tools/version.py')
-with open(ver_path) as ver_file:
-    exec(ver_file.read(), main_ns)
-
 setup(name='DataAnalyticsTools',
-      version=main_ns['__version__'],
+      version=__version__,
       description='Python Distribution Utilities for Data Analytics',
       author='Raul Macedo Fuzita',
       author_email='raul.fuzita@gmail.com',
