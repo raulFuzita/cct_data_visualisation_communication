03b442423a5b985aa7a5ccf269a903964ea0cc84
Raul Macedo Fuzita <raul_fuzita@hotmail.com>
2021-12-04 03:32:47 +0000

    Add files via upload

--- data_analytics_tools/setup.py
+++ data_analytics_tools/setup.py
@@ -1,13 +1,2 @@
-#!/usr/bin/env python
-
-from setuptools import setup, find_packages
-from distutils.util import convert_path
-
-setup(name='DataAnalyticsTools',
-      version=__version__,
-      description='Python Distribution Utilities for Data Analytics',
-      author='Raul Macedo Fuzita',
-      author_email='raul.fuzita@gmail.com',
-      packages=['data_analytics_tools'],
-      package_dir={'data_analytics_tools': 'src/data_analytics_tools'}
-)
+from setuptools import setup
+setup()
